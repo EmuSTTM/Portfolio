206d9e23d3034bb64e622190a7148aedfa19d2bd
Nicolás Perdomo <emusttm5543@gmail.com>
2023-04-30 19:57:08 -0300

    Minificando archivos para deploy

--- src/js/components/MoonSVG.js
+++ src/js/components/MoonSVG.js
@@ -1,6 +1,1 @@
-export const MoonSvg = `
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" width="20" height="20">
-                      <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                  </svg>
-
-                  `
+export const MoonSvg='\n<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" width="20" height="20">\n                      <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>\n                  </svg>\n\n                  ';
